ca95be7da75c006a0c116a429397a19fca760490
BenAmaraYoussra <benamarayousra3@gmail.com>
2024-06-10 16:35:03 +0200

    seconde commit

---
+++ client/src/pages/Profil.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Log from '../components/Log';
+
+const Profil = () => {
+    return (
+        <div className="profil-page">
+        <div className="log-container">
+            <Log />
+            <img src="./img/log.svg" alt="img-log"/>
+        </div>
+        </div>        
+    );
+};
+
+export default Profil;
